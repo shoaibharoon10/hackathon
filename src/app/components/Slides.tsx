44ee75a97acc621a6421585d298f8c3889102707
Muhammmad Shoaib <165128079+shoaibharoon10@users.noreply.github.com>
2024-12-08 22:46:44 +0500

    Update Slides.tsx

--- src/app/components/Slides.tsx
+++ src/app/components/Slides.tsx
@@ -1,28 +1,50 @@
 import Image from "next/image";
-import InnerPeace from "@/assets/inner.png" 
-import SideInner from "@/assets/Sideinner.png" 
-export default function () {
+import InnerPeace from "@/assets/inner.png"; 
+import SideInner from "@/assets/Sideinner.png"; 
+
+const Slides = () => {
   return (
-    <>
-      <div className="slideSection bg-[#FCF8F3]">
-        <div className="slideText pt-9">
-
-        <h4 className=" font-extrabold text-[2rem] ">50+ Beautiful rooms inspiration</h4>
-        <p>
-          Our designer already made a lot of beautiful prototipe of rooms that
-          inspire you
+    <div className="slideSection bg-[#FCF8F3]">
+      {/* Text Section */}
+      <div className="slideText pt-9">
+        <h4 className="font-extrabold text-[2rem]">
+          50+ Beautiful rooms inspiration
+        </h4>
+        <p className="mt-2">
+          Our designer already made a lot of beautiful prototypes of rooms that
+          will inspire you.
         </p>
-        <button className="bg-[#B88E2F] p-2">Exlore More</button>
-        </div>
-        <div className="slideImages">
-        <Image src={InnerPeace} width={250}  alt="Picture of the author" />
+        <button className="bg-[#B88E2F] p-2 mt-4 text-white rounded-md">
+          Explore More
+        </button>
+      </div>
 
+      {/* Image Section */}
+      <div className="slideImages flex justify-center gap-4 mt-6">
+        <div>
+          <Image
+            src={InnerPeace}
+            width={250}
+            height={250}
+            alt="Inner Peace Room Inspiration"
+            className="rounded-lg"
+          />
         </div>
-        <div className="slideImages">
-        <Image src={SideInner} width={250} alt="Picture of the author" />
-
+        <div>
+          <Image
+            src={SideInner}
+            width={250}
+            height={250}
+            alt="Side View of Room Inspiration"
+            className="rounded-lg"
+          />
         </div>
       </div>
-    </>
+    </div>
   );
-}
+};
+
+// Assigning a display name for debugging purposes
+Slides.displayName = "Slides";
+
+export default Slides;
